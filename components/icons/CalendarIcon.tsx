1396fdf28411fd2be41f2eba02290b2063c97414
coolio2156352 <coolio2156352@gmail.com>
2024-11-22 02:49:16 +0700

    feature(CalendarIcon.tsx) remove hyphen

--- components/icons/CalendarIcon.tsx
+++ components/icons/CalendarIcon.tsx
@@ -10,30 +10,30 @@ export const CalendarIcon = ({ width = 14, height = 14 }) => {
       <path
         d="M4.66663 1.1665V3.49984"
         stroke="#616161"
-        stroke-width="1.16667"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.16667"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M9.33337 1.1665V3.49984"
         stroke="#616161"
-        stroke-width="1.16667"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.16667"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M11.0833 2.3335H2.91667C2.27233 2.3335 1.75 2.85583 1.75 3.50016V11.6668C1.75 12.3112 2.27233 12.8335 2.91667 12.8335H11.0833C11.7277 12.8335 12.25 12.3112 12.25 11.6668V3.50016C12.25 2.85583 11.7277 2.3335 11.0833 2.3335Z"
         stroke="#616161"
-        stroke-width="1.16667"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.16667"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M1.75 5.8335H12.25"
         stroke="#616161"
-        stroke-width="1.16667"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.16667"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
