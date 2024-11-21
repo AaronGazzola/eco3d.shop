24a65fdc19fad31a669298e2679f9b509cebefe2
coolio2156352 <coolio2156352@gmail.com>
2024-11-22 05:18:15 +0700

    add(UpDownIcon) add icon

---
+++ components/icons/UpDownIcon.tsx
@@ -0,0 +1,40 @@
+export const UpDownIcon = ({ width = 24, height = 25 }) => {
+  return (
+    <svg
+      width={width}
+      height={height}
+      viewBox="0 0 24 25"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M3 16.5L7 20.5L11 16.5"
+        stroke="#757575"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M7 20.5V4.5"
+        stroke="#757575"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M21 8.5L17 4.5L13 8.5"
+        stroke="#757575"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M17 4.5V20.5"
+        stroke="#757575"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
