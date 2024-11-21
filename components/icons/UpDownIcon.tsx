54d7dae510271798f232311a190cba866f22d33c
coolio2156352 <coolio2156352@gmail.com>
2024-11-22 05:19:39 +0700

    feature(UpDownIcon) remove hyphens

--- components/icons/UpDownIcon.tsx
+++ components/icons/UpDownIcon.tsx
@@ -10,30 +10,30 @@ export const UpDownIcon = ({ width = 24, height = 25 }) => {
       <path
         d="M3 16.5L7 20.5L11 16.5"
         stroke="#757575"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M7 20.5V4.5"
         stroke="#757575"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M21 8.5L17 4.5L13 8.5"
         stroke="#757575"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M17 4.5V20.5"
         stroke="#757575"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
