d43479f9ad6fd16009e05498ac59316b474d2be3
coolio2156352 <coolio2156352@gmail.com>
2024-11-22 02:25:15 +0700

    add(icons) add CalendarIcon

---
+++ components/icons/CalendarIcon.tsx
@@ -0,0 +1,40 @@
+export const CalendarIcon = ({ width = 14, height = 14 }) => {
+  return (
+    <svg
+      width={width}
+      height={height}
+      viewBox="0 0 14 14"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M4.66663 1.1665V3.49984"
+        stroke="#616161"
+        stroke-width="1.16667"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M9.33337 1.1665V3.49984"
+        stroke="#616161"
+        stroke-width="1.16667"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M11.0833 2.3335H2.91667C2.27233 2.3335 1.75 2.85583 1.75 3.50016V11.6668C1.75 12.3112 2.27233 12.8335 2.91667 12.8335H11.0833C11.7277 12.8335 12.25 12.3112 12.25 11.6668V3.50016C12.25 2.85583 11.7277 2.3335 11.0833 2.3335Z"
+        stroke="#616161"
+        stroke-width="1.16667"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M1.75 5.8335H12.25"
+        stroke="#616161"
+        stroke-width="1.16667"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
