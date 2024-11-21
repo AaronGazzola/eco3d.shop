3181cd655ad8be9dd63d64d08599740ab93dc5f5
coolio2156352 <coolio2156352@gmail.com>
2024-11-22 02:30:52 +0700

    fix(Header.tsx) set z-index to be over next/image

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -20,7 +20,7 @@ const Header = () => {
     <>
       <header
         className={cn(
-          "sticky top-0 left-0 right-0 w-full min-h-12 flex items-stretch shadow-md justify-center bg-background py-0.5 z-100",
+          "sticky top-0 left-0 right-0 w-full min-h-12 flex items-stretch shadow-md justify-center bg-background py-0.5 z-50",
         )}
       >
         <div className="flex h-full justify-between max-w-[100rem] flex-grow pr-0.5 items-stretch">
