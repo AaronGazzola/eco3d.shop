611f7c02d41745c87ed9af1b73c5a7d3c02d6be2
Aaron Gazzola <aaron@gazzola.dev>
2024-11-25 23:45:42 +1100

    feature(/[product_slug]): Apply product page layout styling

--- app/(main)/[product_slug]/page.tsx
+++ app/(main)/[product_slug]/page.tsx
@@ -15,72 +15,149 @@ import {
   Pencil,
   Ruler,
   ShoppingBasket,
-  SquareMousePointer,
 } from "lucide-react";
 import { useState } from "react";
 
-const { Customise, Personalise, AddToCart } = AddToCartStep;
+const { Customise, Personalise, AddToCart, Select } = AddToCartStep;
+
+const BreadcrumbItemComponent = ({
+  href,
+  activeStep,
+  step,
+  onClick,
+}: {
+  href?: string;
+  isComplete?: boolean;
+  step: AddToCartStep;
+  onClick?: () => void;
+  activeStep: AddToCartStep;
+}) => {
+  const isComplete =
+    step === Select ||
+    (step === Customise && activeStep !== Customise) ||
+    (step === Personalise &&
+      activeStep !== Personalise &&
+      activeStep !== Customise);
+
+  const isActive = step === activeStep;
+  const isDisabled = step !== activeStep && !isComplete;
+  const icon =
+    step === Customise ? (
+      <Ruler className="w-4 h-4 mt-px" />
+    ) : step === Personalise ? (
+      <Pencil className="w-4 h-4 mt-px" />
+    ) : step === AddToCart ? (
+      <ShoppingBasket className="w-5 h-5 -mt-px" />
+    ) : null;
+  if (isComplete) console.log({ step, activeStep });
+  if (isComplete)
+    return (
+      <BreadcrumbItem onClick={onClick}>
+        <BreadcrumbLink
+          href={href}
+          className="text-base text-green-900 cursor-pointer font-medium"
+        >
+          <div className="flex items-center gap-2 rounded bg-green-500/5 px-1.5 pb-px">
+            <Check className="w-4 h-4 mt-px" />
+            <span>{step}</span>
+          </div>
+        </BreadcrumbLink>
+      </BreadcrumbItem>
+    );
+  if (isActive)
+    return (
+      <BreadcrumbItem className="cursor-default" onClick={onClick}>
+        {/* <div className="absolute bottom-0 left-0 right-0 border-b border-green-800/80 transform translate-y-1"></div> */}
+        <BreadcrumbLink className="font-bold text-base text-black">
+          <div className="flex items-center gap-1.5 rounded-lg pl-1.5 pr-2 pb-px font-medium border border-green-900/50">
+            {icon} {step}
+          </div>
+        </BreadcrumbLink>
+      </BreadcrumbItem>
+    );
+  if (isDisabled)
+    return (
+      <BreadcrumbItem>
+        <BreadcrumbLink className="text-medium text-gray-500 text-base hover:text-gray-500 cursor-not-allowed">
+          <div className="flex items-center gap-1.5">
+            {icon} {step}
+          </div>
+        </BreadcrumbLink>
+      </BreadcrumbItem>
+    );
+  return null;
+};
 
 const Page = () => {
   const [activeStep, setActiveStep] = useState(Customise);
   return (
     <div className="flex flex-col items-stretch w-full overflow-hidden flex-grow">
-      <div className="hover:bg-gray-200 w-full flex items-center group px-4 py-2 space-x-2 bg-gradient-to-l from-white to-gray-50">
+      <div className="w-full flex items-center group px-4 py-2 space-x-2 shadow">
         <Breadcrumb>
           <BreadcrumbList>
-            <BreadcrumbItem>
-              <BreadcrumbLink
-                href={configuration.paths.appHome}
-                className="text-green-900"
-              >
-                <div className="flex items-center gap-2 rounded bg-green-500/5 px-1.5 pb-px">
-                  <SquareMousePointer className="w-4 h-4 mt-px" />
-                  <span className="text-black">Select</span>
-                  <Check className="w-4 h-4 mt-px" />
-                </div>
-              </BreadcrumbLink>
-            </BreadcrumbItem>
+            <BreadcrumbItemComponent
+              isComplete
+              href={configuration.paths.appHome}
+              step={Select}
+              activeStep={activeStep}
+            />
+
             <ChevronRight className="w-5 mt-px" />
-            <BreadcrumbItem className="relative">
-              <div className="absolute bottom-0 left-0 right-0 border-b border-gray-400 transform translate-y-1"></div>
-              <BreadcrumbLink className="text-black cursor-default">
-                <div className="flex items-center gap-1.5 rounded pl-1 pr-2 pb-px">
-                  <Ruler className="w-4 h-4 mt-px" /> Customise
-                </div>
-              </BreadcrumbLink>
-            </BreadcrumbItem>
+            <BreadcrumbItemComponent
+              activeStep={activeStep}
+              step={Customise}
+              onClick={() => setActiveStep(Customise)}
+            />
             <ChevronRight className="w-5 mt-px" />
-            <BreadcrumbItem>
-              <BreadcrumbLink>
-                <div className="flex items-center gap-1.5">
-                  <Pencil className="w-4 h-4 mt-px" /> Personalise
-                </div>
-              </BreadcrumbLink>
-            </BreadcrumbItem>
+
+            <BreadcrumbItemComponent
+              activeStep={activeStep}
+              step={Personalise}
+              onClick={() => setActiveStep(Personalise)}
+            />
             <ChevronRight className="w-5 mt-px" />
-            <BreadcrumbItem>
-              <BreadcrumbLink>
-                <div className="flex items-center gap-1.5">
-                  <ShoppingBasket className="w-5 h-5 -mt-px" /> Add to cart
-                </div>
-              </BreadcrumbLink>
-            </BreadcrumbItem>
+
+            <BreadcrumbItemComponent
+              activeStep={activeStep}
+              step={AddToCart}
+              onClick={() => setActiveStep(AddToCart)}
+            />
           </BreadcrumbList>
         </Breadcrumb>
       </div>
-      {[Customise, Personalise, AddToCart].map(step => (
-        <div
-          key={step}
-          onClick={() => setActiveStep(step)}
-          className={cn(
-            "transition-all ease duration-1000 relative overflow-hidden",
-            step === activeStep ? "flex-grow min-h-[2.5rem]" : "h-10",
-          )}
-        >
-          <div className={cn("absolute inset-0")}>
-            <div className="p-4 h-full">
-              <div className="sticky top-0 flex flex-col">
-                <h2 className="text-lg font-semibold">{step} Section</h2>
+      {[Customise, Personalise, AddToCart].map(step => {
+        const isCustomise = step === Customise;
+        const isPersonalise = step === Personalise;
+        const isAddToCart = step === AddToCart;
+        const isActive = step === activeStep;
+        const icon =
+          step === Customise ? (
+            <Ruler className="w-8 h-8 mt-px" />
+          ) : step === Personalise ? (
+            <Pencil className="w-8 h-8 mt-px" />
+          ) : step === AddToCart ? (
+            <ShoppingBasket className="w-10 h-10 -mt-px" />
+          ) : null;
+        return (
+          <div
+            key={step}
+            onClick={() => setActiveStep(step)}
+            className={cn(
+              "transition-all ease-out duration-500 relative overflow-hidden cursor-pointer",
+              isActive ? "flex-grow min-h-[92px]" : "h-[92px]",
+            )}
+          >
+            <div className={cn("absolute inset-0")}>
+              <div
+                className={cn(
+                  "flex-grow p-4 flex items-center gap-4 text-gray-800",
+                  isActive && "text-green-800",
+                )}
+              >
+                {icon}
+                <h1 className="text-[2.5rem] font-semibold ">{step}</h1>
+              </div>
+              <div className="pb-4">
                 <p className="mt-2">
                   This is the content for the {step} section. It expands to fill
                   the available space and scrolls if the content overflows.
@@ -102,8 +179,8 @@ const Page = () => {
               </div>
             </div>
           </div>
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
 };
