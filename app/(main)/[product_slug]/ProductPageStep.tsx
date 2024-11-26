af44b33a328b149cca6bda0e12f4fe7ce72786c8
Aaron Gazzola <aaron@gazzola.dev>
2024-11-27 10:26:28 +1100

    feature(product page): refactor styling and animation of product page

--- app/(main)/[product_slug]/ProductPageStep.tsx
+++ app/(main)/[product_slug]/ProductPageStep.tsx
@@ -1,6 +1,6 @@
 import { cn } from "@/lib/utils";
 import { AddToCartStep } from "@/types/ui.types";
-import { Pencil, Ruler, ShoppingBasket } from "lucide-react";
+import { ChevronDown, Pencil, Ruler, ShoppingBasket } from "lucide-react";
 
 const { Customise, Personalise, AddToCart, Select } = AddToCartStep;
 
@@ -10,36 +10,59 @@ const ProductPageStep = ({
   step,
   activeStep,
   onChangeActiveStep,
+  isDisabled: isDisabledProp = false,
 }: {
   step: AddToCartStep;
   activeStep: AddToCartStep;
   onChangeActiveStep: (step: AddToCartStep) => void;
+  isDisabled?: boolean;
 }) => {
-  const isCustomise = step === Customise;
-  const isPersonalise = step === Personalise;
-  const isAddToCart = step === AddToCart;
   const isActive = step === activeStep;
+  const isNext =
+    (step === Personalise && activeStep === Customise) ||
+    (step === AddToCart && activeStep === Personalise);
+  const isDisabled =
+    isDisabledProp || (step === AddToCart && activeStep === Customise);
+  const isComplete =
+    (step === Personalise && activeStep === AddToCart) ||
+    (step === Customise &&
+      (activeStep === Personalise || activeStep === AddToCart));
+
+  const fontColorClass = isDisabled
+    ? "text-gray-500"
+    : isNext
+      ? "text-gray-50 hover:text-white"
+      : "";
   const icon =
     step === Customise ? (
-      <Ruler className="w-7 h-7 mt-px" />
+      <Ruler className={cn("w-7 h-7 mt-px", fontColorClass)} />
     ) : step === Personalise ? (
-      <Pencil className="w-7 h-7 mt-px" />
+      <Pencil className={cn("w-7 h-7 mt-px", fontColorClass)} />
     ) : step === AddToCart ? (
-      <ShoppingBasket className="w-9 h-9 -mt-px" />
+      <ShoppingBasket className={cn("w-9 h-9 -mt-px", fontColorClass)} />
     ) : null;
   return (
     <div
       key={step}
-      onClick={() => onChangeActiveStep(step)}
+      onClick={() => !isDisabled && onChangeActiveStep(step)}
       style={{
         minHeight: COLLAPSED_HEIGHT,
       }}
       className={cn(
-        "transition-all ease-out duration-500 relative overflow-hidden cursor-pointer shadow-md",
+        "transition-all ease-in-out duration-700 relative overflow-hidden cursor-pointer shadow-md",
         isActive && "flex-grow",
       )}
     >
-      <div className={cn("absolute inset-0 flex justify-center")}>
+      <div
+        className={cn(
+          "absolute inset-0 flex justify-center group",
+          isNext && "bg-green-700/90 hover:bg-green-700",
+          isDisabled && "bg-gray-200 && cursor-not-allowed",
+        )}
+        style={{
+          maxHeight: isActive ? "100vh" : COLLAPSED_HEIGHT,
+        }}
+      >
         <div className="w-full max-w-7xl flex-grow ">
           <div
             className={cn(
@@ -48,27 +71,13 @@ const ProductPageStep = ({
             )}
           >
             {icon}
-            <h1 className="text-[2rem] font-semibold ">{step}</h1>
-          </div>
-          <div className="pb-4">
-            <p className="mt-2">
-              This is the content for the {step} section. It expands to fill the
-              available space and scrolls if the content overflows.
-            </p>
-            <div className="mt-4 space-y-2">
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
-                lacinia odio vitae vestibulum vestibulum.
-              </p>
-              <p>
-                Cras venenatis euismod malesuada. Nullam ac urna eu felis
-                dapibus condimentum sit amet a augue.
-              </p>
-              <p>
-                Donec sodales, nisi id mattis convallis, sem velit facilisis
-                ipsum, eget mollis orci odio id libero.
-              </p>
-            </div>
+            <h1 className={cn("text-[2rem] font-semibold", fontColorClass)}>
+              {step}
+            </h1>
+            {/* TODO: add order details summary related completed step  */}
+            {isNext && !isDisabled && (
+              <ChevronDown className="w-7 h-7 mt-1 text-white" />
+            )}
           </div>
         </div>
       </div>
