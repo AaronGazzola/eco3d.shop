046832bbd119e4fc4e7bb35c09c75e57da4ca210
coolio2156352 <coolio2156352@gmail.com>
2024-11-27 13:54:48 +0700

    Update(CartItem type) add description field

--- types/db.types.ts
+++ types/db.types.ts
@@ -4,7 +4,8 @@ export interface CartItem {
   id: number;
   name: string;
   price: number;
-  quantity: number;
+  quantity?: number;
+  description: string;
 }
 
 export interface HookOptions<T, K = { id: string }> {
