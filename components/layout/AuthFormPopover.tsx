f93893071ad793bc42f9b0087c6bfe02c0e9fda1
Aaron Gazzola <aaron@gazzola.dev>
2024-12-17 19:09:25 +1100

    feature(auth form popover): add sign out and view orders buttons to auth form popover

--- components/layout/AuthFormPopover.tsx
+++ components/layout/AuthFormPopover.tsx
@@ -1,4 +1,5 @@
 import ActionButton from "@/components/layout/ActionButton";
+import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
@@ -9,6 +10,9 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import ConfirmDialog from "@/components/ux/ConfirmDialog";
+import configuration from "@/configuration";
+import { useUIStore } from "@/hooks/useUIStore";
 import {
   useGetUser,
   useSignInWithMagicLink,
@@ -18,9 +22,12 @@ import { maskEmail } from "@/lib/string.util";
 import { cn } from "@/lib/utils";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Popover } from "@radix-ui/react-popover";
-import { CircleUser } from "lucide-react";
+import { CircleUser, LogOut, PackageOpen } from "lucide-react";
+import Link from "next/link";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
+
 const formSchema = z.object({
   email: z.string().email({ message: "Invalid email address" }),
 });
@@ -30,17 +37,26 @@ const AuthFormPopover = () => {
   const { mutate: signOut, isPending: signOutIsPending } = useSignOut();
   const { mutate: signIn, isPending: signInIsPending } =
     useSignInWithMagicLink();
+  const [showSignOutConfirm, setShowSignOutConfirm] = useState(false);
+
+  const { toggleDrawer } = useUIStore();
+
   const form = useForm({
     resolver: zodResolver(formSchema),
     defaultValues: {
       email: "",
     },
   });
+
   const onSubmit = (values: { email: string }) => {
     signIn(values.email);
   };
 
   const isPending = getUserIsPending || signOutIsPending || signInIsPending;
+
+  const handleSignOut = () => {
+    setShowSignOutConfirm(true);
+  };
 
   return (
     <Popover modal>
@@ -55,17 +71,37 @@ const AuthFormPopover = () => {
           <CircleUser className="" />
         </ActionButton>
       </PopoverTrigger>
-      <PopoverContent className="border border-gray-500">
+      <PopoverContent className="w-min border border-gray-500 text-gray-900 px-6 py-4 min-w-80">
         {user ? (
-          <div className="flex flex-col items-center">
-            {user.email && <h2 className="">{maskEmail(user.email)}</h2>}
-            <ActionButton
-              onClick={() => signOut()}
-              loading={isPending}
-              className="w-full"
+          <div className="flex flex-col items-center py-2">
+            <div className="flex flex-col items-center">
+              {user.email && (
+                <h2 className="font-semibold text-gray-700 italic">
+                  {maskEmail(user.email)}
+                </h2>
+              )}
+              <Button
+                variant="outline"
+                size="sm"
+                onClick={handleSignOut}
+                className="text-gray-500 whitespace-nowrap flex items-center gap-2 py-1 px-2.5 hover:text-red-500 hover:border-red-500 hover:bg-transparent border-transparent mt-2"
+              >
+                Sign Out <LogOut className="w-4 h-4" />
+              </Button>
+            </div>
+            <Button
+              variant="outline"
+              className="mt-4 border-secondary text-secondary text-base "
             >
-              Sign out
-            </ActionButton>
+              <Link
+                href={configuration.paths.me.path}
+                className="flex items-center justify-center"
+                onClick={() => toggleDrawer(false)}
+              >
+                <PackageOpen className="w-5 h-5 mr-3" />
+                <span>My orders</span>
+              </Link>
+            </Button>
           </div>
         ) : (
           <Form {...form}>
@@ -75,7 +111,7 @@ const AuthFormPopover = () => {
                 name="email"
                 render={({ field }) => (
                   <FormItem>
-                    <FormLabel> Sign in with magic link:</FormLabel>
+                    <FormLabel>Sign in with magic link:</FormLabel>
                     <FormControl>
                       <Input placeholder="Enter your email" {...field} />
                     </FormControl>
@@ -94,6 +130,18 @@ const AuthFormPopover = () => {
           </Form>
         )}
       </PopoverContent>
+      <ConfirmDialog
+        title="Sign Out"
+        description="Are you sure you want to sign out?"
+        onConfirm={() => {
+          setShowSignOutConfirm(false);
+          signOut();
+        }}
+        onCancel={() => setShowSignOutConfirm(false)}
+        confirmText="Sign Out"
+        cancelText="Cancel"
+        open={showSignOutConfirm}
+      />
     </Popover>
   );
 };
