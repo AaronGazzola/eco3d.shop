f6134d266257abe0e61e93b109db9c0f1fa31a3e
Aaron Gazzola <aaron@gazzola.dev>
2024-12-21 10:09:44 +1100

    task(/me):title

--- app/(main)/me/page.tsx
+++ app/(main)/me/page.tsx
@@ -4,6 +4,13 @@ import { useUserOrdersQuery } from "@/hooks/orderHooks";
 
 const Page = () => {
   const { data } = useUserOrdersQuery();
-  return <OrdersPage orders={data} />;
+  return (
+    <div className="flex flex-col items-center">
+      <h1 className="text-2xl font-bold px-6 pb-4 pt-8 w-full max-w-3xl ">
+        My Orders
+      </h1>
+      <OrdersPage orders={data} />
+    </div>
+  );
 };
 export default Page;
