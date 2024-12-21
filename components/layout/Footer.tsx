16d5c7dfec3af4a241173c710276d06d0396dc63
Aaron Gazzola <aaron@gazzola.dev>
2024-12-22 09:50:59 +1100

    reduce footer text size

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <div className="w-full bg-gradient-to-r from-green-900 via-green-950 to-green-900 flex items-center justify-center text-gray-200 font-black text-sm shadow relative flex-cold">
       <div className="hidden md:absolute md:inset-0 md:flex items-center md:justify-end pr-2 font-normal pointer-events-none">
-        <span className="text-white">
+        <span className="text-white text-xs">
           Copyright Apex Apps © {new Date().getFullYear()}
         </span>
       </div>
